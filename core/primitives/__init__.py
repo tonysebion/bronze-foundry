1983f974044492af8d54733cccffbab15bf2ca24
Tony Sebion <tony@sebion.com>
2025-12-07 05:21:45 -0600

    refactor: remove backward compatibility shims for core modules and update imports to their new locations

--- core/primitives/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Backward compatibility shim for core.primitives."""
-from core._compat.primitives import *  # noqa: F401,F403
-from core._compat.primitives import __all__ as _compat_all
-
-__all__ = list(_compat_all)
